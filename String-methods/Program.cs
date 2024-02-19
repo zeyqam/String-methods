dc6451d66f87ff31a3e9066a9976232760958bf4
zeyqam <zegamda@code.edu.az>
2024-02-20 01:33:42 +0400

    hometask

---
+++ String-methods/Program.cs
@@ -0,0 +1,12 @@
+﻿//Parametr olaraq string qebul eden metod yazmalisiz. Hemin metod qebul etdiyi stringi tersine formada ekranda gostersin. (Cavid gelirse , ekranda divaC gorsensin).
+
+//void ReverseString(string input)
+//{
+//    char[] charArray = input.ToCharArray();
+//    Array.Reverse(charArray);
+//    string reversedString=new string(charArray);
+//    Console.WriteLine("Reversed string: " + reversedString);
+//}
+//Console.WriteLine("Please enter string: ");
+//string input=Console.ReadLine();
+//ReverseString(input);
